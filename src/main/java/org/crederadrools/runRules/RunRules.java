e44f8f0000edca1fd3de2ca43fb66bace6b2c9e5
Connor Riddell <connor.riddell@sita.aero>
2024-02-09 15:21:53 +0000

    edit

--- src/main/java/org/crederadrools/runRules/RunRules.java
+++ src/main/java/org/crederadrools/runRules/RunRules.java
@@ -4,12 +4,21 @@ import org.crederadrools.builder.TransactionsRepository;
 import org.crederadrools.facts.Account;
 import org.crederadrools.facts.Transaction;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+
 import java.util.List;
 
 public class RunRules {
     public static void main(final String[] args) {
-        List<Account> accountsList = AccountRepository.getAccounts();
-        System.out.println("This is the accountsList:");
-        System.out.println(accountsList);
+        KieContainer kieClasspathContainer = KieServices.Factory.get().getKieClasspathContainer();
+        KieSession ksession = kieClasspathContainer.newKieSession("StatelessFraudCheckdemo1");
+        List<Account> accounts = AccountRepository.getAccounts();
+        System.out.println(accounts);
+        accounts.forEach(ksession::insert);
+
+        ksession.fireAllRules();
     }
 }
